959a10c78fc7b7eb57d577c9685bfb75ebf289a0
akiya <akiya.yang@pomelofun.com>
2026-02-10 16:13:00 +0800

    refactor: update SocialIcon labels to use dynamic content

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -34,11 +34,11 @@ export default function AuthorLayout({ children, content }: Props) {
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex flex-col space-y-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} size={5} label="Email" />
-              <SocialIcon kind="github" href={github} size={5} label="GitHub" />
-              <SocialIcon kind="linkedin" href={linkedin} size={5} label="LinkedIn" />
-              <SocialIcon kind="x" href={twitter} size={5} label="X" />
-              <SocialIcon kind="bluesky" href={bluesky} size={5} label="Bluesky" />
+              <SocialIcon kind="mail" href={`mailto:${email}`} size={5} label={email} />
+              <SocialIcon kind="github" href={github} size={5} label={github} />
+              <SocialIcon kind="linkedin" href={linkedin} size={5} label={linkedin} />
+              <SocialIcon kind="x" href={twitter} size={5} label={twitter} />
+              <SocialIcon kind="bluesky" href={bluesky} size={5} label={bluesky} />
             </div>
           </div>
           <div className="prose dark:prose-invert max-w-none pt-8 pb-8 xl:col-span-2">
